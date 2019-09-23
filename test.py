da3dee12714af164761373009ba3bbb46136e383
Rahul Tholakapalli <rahultholakapalli@gmail.com>
2019-09-23 03:03:31 -0400

    Wip filtering messages with both emotes and text

--- test.py
+++ test.py
@@ -19,13 +19,20 @@ sock.send(f"JOIN {channel}\n".encode('utf-8'))
 
 unpickled = pd.read_pickle('ICWSM19_data/ninja.pkl')
 frags = unpickled['fragments']
+
 for items in frags.iteritems():
     # print(items[1])
     data = pd.DataFrame(items[1])
+    # print(items[1])
     # data = [json.loads(i) for i in data if i]    #Iterate your list check if you have data then use json.loads
-    print("\nnewframe")
-    print(items[1])
-    print(data)
+    # print("\nnewframe")
+    if('emoticon_id' in data and 'text' in data):
+        # print(data[data.emoticon_id.notnull()])
+        data2 = data[data['text'] != ' ']
+        data3 = data2[data['text'].notnull()]
+        if not data3.empty:
+            print(items[1])
+            print(data3)
 
 
 # print(type(frags))
