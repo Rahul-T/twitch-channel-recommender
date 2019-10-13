772b7d3df3c47067903ba3bd3b39dde2b8016efd
Rahul Tholakapalli <rahultholakapalli@gmail.com>
2019-10-12 20:42:44 -0400

    Created emote to word to emotion mapping with default value

--- harvarddata.py
+++ harvarddata.py
@@ -3,39 +3,18 @@ import pandas as pd
 from emotedict import *
 import datetime
 import twitter_emotion_recognition.emotion_predictor as twitter
+from collections import Counter 
+import os
 
-model = twitter.EmotionPredictor(classification='ekman', setting='mc', use_unison_model=True)
-# tweets = [
-#     "lmao",
-#     "haha",
-#     "lol",
-#     "this game sucks",
-#     "I'm dead",
-#     "No",
-#     "lmaoooooooooo",
-#     "LOL",
-#     "lmaooo",
-#     "lmaoo",
-#     "lmaoooo",
-#     "wtf",
-#     "WTF",
-#     "LUL",
-#     "Let's go",
-#     "LET'S GO",
-#     "LET'S GOOOOOOOOO",
-#     "LET'S GOOOOOOOO!",
-#     "these kids are retarded",
-#     "you fucking suck",
-#     "i hate these guys"
-# ]
+# model = twitter.EmotionPredictor(classification='ekman', setting='mc', use_unison_model=True)
 
-# predictions = model.predict_classes(tweets)
-# print(predictions, '\n')
-# predictions = model.predict_classes(tweets)
-# print(predictions, '\n')
 emoteToWordCount = {}
+emoteToEmotionCount = {}
 
 def readData():
+    # for filename in os.listdir('ICWSM19_data'):
+    #     print(filename)
+
     then = datetime.datetime.now()
     unpickled = pd.read_pickle('ICWSM19_data/ninja.pkl')
     frags = unpickled['fragments']
@@ -43,7 +22,7 @@ def readData():
 
     for items in frags.iteritems():
         data = pd.DataFrame(items[1])
-        if limit == 100:
+        if limit == 0:
             break
         if('emoticon_id' in data and 'text' in data):
             validText = data[(data['text'] != ' ') & data['text'].notnull()]
@@ -61,6 +40,7 @@ def readData():
                         emotesInMessage.add(emoteId)
                         if emote not in emoteToWordCount:
                             emoteToWordCount[emote] = {}
+                            emoteToEmotionCount[emote] = {}
                         for row in validText.head().itertuples():
                             words = removeNoise(row[2])
                             strList = []
@@ -68,23 +48,42 @@ def readData():
                             # print(strList)
                             predictions = model.predict_classes(strList)
                             mood = predictions.values[0][1]
-                            print("Emote: ", emote, "Mood: ", mood)
+                            # print("Emote: ", emote, "Mood: ", mood)
                             for word in words:
                                 if word not in emoteToWordCount[emote]:
                                     emoteToWordCount[emote][word] = 1
+                                    emoteToEmotionCount[emote][word] = {'Anger': 0, 'Disgust': 0, 'Fear': 0, 'Joy': 0, 'Sadness': 0, 'Surprise': 0}
+                                    emoteToEmotionCount[emote][word][mood] = 1
                                 else:
                                     emoteToWordCount[emote][word] = emoteToWordCount[emote][word] + 1
+                                    emoteToEmotionCount[emote][word][mood] = emoteToEmotionCount[emote][word][mood] + 1
                                 # emoteToWordCount[emote][word] = (emoteToWordCount[emote]).get(word, 0) + 1 
                                 # print("emote: ", idsToEmotes[emote], " word: ", word, " count: ", emoteToWordCount[emote][word])
                                 # print(row[2])
-    for key in emoteToWordCount:
+    for emote in emoteToWordCount:
         # print(key)
-        srtd = sorted(emoteToWordCount[key], key=emoteToWordCount[key].get, reverse=True)
-        emoteToWordCount[key] = srtd[:10]
+        srtd = sorted(emoteToWordCount[emote], key=emoteToWordCount[emote].get, reverse=True)
+        emoteToWordCount[emote] = srtd[:10]
+
+    for emote in emoteToEmotionCount.copy():
+        for word in emoteToEmotionCount[emote].copy():
+            if word not in emoteToWordCount[emote]:
+                del emoteToEmotionCount[emote][word]
+
+    emoteToBestEmotion = {}
+    for emote in emoteToEmotionCount:
+        emoteToBestEmotion[emote] = {}
+        for word in emoteToEmotionCount[emote]:
+            emoteToBestEmotion[emote][word] = max(emoteToEmotionCount[emote][word], key=emoteToEmotionCount[emote][word].get)
+        if(emoteToBestEmotion[emote]):
+            value, count = Counter(emoteToBestEmotion[emote].values()).most_common(1)[0]
+            emoteToBestEmotion[emote][None] = value
+
+
     print(emoteToWordCount)
+    print("\nBREAK\n")
+    print(emoteToEmotionCount)
+    print("\nBREAK\n")
+    print(emoteToBestEmotion)
     print("Start: ", then)
     print("End: ", datetime.datetime.now())
-
-                        # print(row[2])
-                            # print(row[1])
-                            # print(idsToEmotes.get(int(row[1])))
