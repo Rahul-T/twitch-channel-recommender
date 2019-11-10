dad73417f708a84d59d3826d009666e0bef22448
Rahul Tholakapalli <rahultholakapalli@gmail.com>
2019-11-10 17:48:37 -0500

    Added basic components

---
+++ frontend/src/components/Recommendation.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Recommendation extends Component {
+    render() {
+        return (<div>
+            Recommendations displayed here
+        </div>)
+    }
+}
+
+export default Recommendation
