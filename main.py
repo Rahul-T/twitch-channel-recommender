b7296eeba1494c7ee947619d3a04950172ee5fbf
Rahul Tholakapalli <rahultholakapalli@gmail.com>
2019-11-09 21:21:17 -0500

    Finished first version, added overrides, filtering

--- main.py
+++ main.py
@@ -1,42 +1,85 @@
 import socket
 import logging
 import os
-from cleantext import *
-from harvarddata import *
-from emotedict import *
-from machinelearning import *
+import re
+import requests
+from twitch import TwitchHelix
+from itertools import islice
+from realtimeanalysis import analyzeMessage
 
 server = 'irc.chat.twitch.tv'
 port = 6667
 nickname = os.environ['TWITCH_USERNAME']
 token = os.environ['OAUTH']
-channel = '#cyn1c'
+client = TwitchHelix(client_id=os.environ['CLIENT_ID'])
 
-sock = socket.socket()
-sock.connect((server, port))
+def mapGamesToIds():
+    gamesToIds = {}
+    games_iterator = client.get_top_games(page_size=3)
+    for game in islice(games_iterator, 0, 5):
+        gamesToIds[game['name']] = game['id']
+    print(gamesToIds)
+    return gamesToIds
 
-sock.send("PASS {}\n".format(token).encode('utf-8'))
-sock.send("NICK {}\n".format(nickname).encode('utf-8'))
-sock.send("JOIN {}\n".format(channel).encode('utf-8'))
+def filterStreams(gameId):
+    channels = []
+    streams_iterator = client.get_streams()
+    streamCount = 0
 
-def getLiveData(ed):
+    for stream in islice(streams_iterator, 0, None):
+        if stream['language'] != 'en' or (gameId is not None and gameId != stream['game_id']):
+            continue
+        channels.append('#' + stream['user_name'].lower())
+        streamCount += 1
+        if(streamCount == 5):
+            break
+
+    print(channels)
+    return channels
+
+def getChannelEmotions(channel):
+    sock = socket.socket()
+    sock.connect((server, port))
+    sock.send("PASS {}\n".format(token).encode('utf-8'))
+    sock.send("NICK {}\n".format(nickname).encode('utf-8'))
+    sock.send("JOIN {}\n".format(channel).encode('utf-8'))
+
+    currentEmotions = {'Anger': 0, 'Disgust': 0, 'Fear': 0, 'Joy': 0, 'Sadness': 0, 'Surprise': 0}
     for x in range(100):
         resp = sock.recv(2048).decode('utf-8')
         message = re.search(':(.*)', resp[1:])
         if not message is None:
-            msg = message.group(0)[1:-1].split(' ')
-            print(msg)
-            for word in msg:
-                if word in ed:
-                    print(ed[word])
+            msg = message.group(0)[1:-1]
+            print("Message: ", msg)
+            emotion = analyzeMessage(msg)
+            if emotion is not None:
+                currentEmotions[emotion] += 1
+    print(currentEmotions)
+    return currentEmotions
 
-# with open("emoteToBestEmotion.pkl","rb") as f:
-#     emoteToBestEmotion = pickle.load(f)
-# print(emoteToBestEmotion)
+def analyzeStreams(game, gamesToIds):
+    if game is not None:
+        game = gamesToIds[game]
 
-# trainModel()
-# emoteCluster()
-# emotesToIds, idsToEmotes = buildEmoteDicts()
-# print(emotesToIds.get('Kappa'))
-# print(idsToEmotes.get(1))
-# getLiveData(ed)
+    channels = filterStreams(game)
+
+    channelEmotions = {}
+    # channels = ['#summit1g', '#nickmercs']
+    for channel in channels:
+        channelEmotions[channel] = getChannelEmotions(channel)
+    print(channelEmotions)
+    return channelEmotions
+
+def main():
+    gamesToIds = mapGamesToIds()
+    game = None
+    emotion = 'Joy'
+    channelEmotions = analyzeStreams(game, gamesToIds)
+    bestStream = max(channelEmotions, key=lambda channel: channelEmotions[channel][emotion])
+    print(bestStream[1:])
+
+# analyzeStreams()
+# getLiveData()
+# filterStreams()
+# mapGamesToIds()
+main()
