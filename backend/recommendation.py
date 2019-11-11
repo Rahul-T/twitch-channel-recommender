7fcf8495e8fe6369f6d832307deffea7c70d6565
Rahul Tholakapalli <rahultholakapalli@gmail.com>
2019-11-11 13:13:56 -0500

    Added multithreading for receiving messages

--- backend/recommendation.py
+++ backend/recommendation.py
@@ -1,4 +1,5 @@
 import socket
+import concurrent.futures
 import logging
 import os
 import re
@@ -37,36 +38,65 @@ def filterStreams(gameId):
     print(channels)
     return channels
 
-def getChannelEmotions(channel):
+def getLiveMessages(channel):
     sock = socket.socket()
     sock.connect((server, port))
     sock.send("PASS {}\n".format(token).encode('utf-8'))
     sock.send("NICK {}\n".format(nickname).encode('utf-8'))
     sock.send("JOIN {}\n".format(channel).encode('utf-8'))
 
-    currentEmotions = {'Anger': 0, 'Disgust': 0, 'Fear': 0, 'Joy': 0, 'Sadness': 0, 'Surprise': 0}
+    # currentEmotions = {'Anger': 0, 'Disgust': 0, 'Fear': 0, 'Joy': 0, 'Sadness': 0, 'Surprise': 0}
+    messages = []
+
     for x in range(50):
         resp = sock.recv(2048).decode('utf-8')
         message = re.search(':(.*)', resp[1:])
         if not message is None:
             msg = message.group(0)[1:-1]
-            print("Message: ", msg)
-            emotion = analyzeMessage(msg)
-            if emotion is not None:
-                currentEmotions[emotion] += 1
+            print("Message: ", msg, "Channel: ", channel)
+            messages.append(msg)
+            # emotion = analyzeMessage(msg)
+            # emotion = None
+            # if emotion is not None:
+            #     currentEmotions[emotion] += 1
+    print("channel:", channel, "messages:", messages)
+    # print(messages)
+    # return currentEmotions
+    return messages
+
+def getChannelEmotions(channelMessages):
+    currentEmotions = {'Anger': 0, 'Disgust': 0, 'Fear': 0, 'Joy': 0, 'Sadness': 0, 'Surprise': 0}
+
+    for msg in channelMessages:
+        emotion = None
+        emotion = analyzeMessage(msg)
+        if emotion is not None:
+            currentEmotions[emotion] += 1
+    
     print(currentEmotions)
     return currentEmotions
-
+    
 def analyzeStreams(game, gamesToIds):
     if game is not None:
         game = gamesToIds[game]
 
     channels = filterStreams(game)
 
-    channelEmotions = {}
+    channelMessages = {}
+
     # channels = ['#summit1g', '#nickmercs']
-    for channel in channels:
-        channelEmotions[channel] = getChannelEmotions(channel)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        allMessages = executor.map(getLiveMessages, channels)
+        channelIndex = 0
+        for channelMessage in allMessages:
+            # print("Channel: ", channels[channelIndex], "RESULTS:", i)
+            channelMessages[channels[channelIndex]] = channelMessage
+            channelIndex += 1
+
+    channelEmotions = {}
+    for channel in channelMessages:
+        channelEmotions[channel] = getChannelEmotions(channelMessages[channel])
+
     print(channelEmotions)
     return channelEmotions
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { Route, BrowserRouter, Switch } from 'react-router-dom'
 import Home from './components/Home.js'
-import Recommendation from './components/Recommendation'
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <div className="App">
           <Switch>
             <Route exact path='/' component={Home}/>
-            <Route path='/recommendation' component={Recommendation} />
           </Switch>
       </div>
     </BrowserRouter>
